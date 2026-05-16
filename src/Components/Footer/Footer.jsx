aa27b31cc152449c52ffb15cf16049683cc7e4df
kareemHabeeb <omogbolahankareem16@gmail.com>
2026-05-16 16:06:29 +0100

    Landing Page

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="Foot">
+      <img className="FooterImg" src="src/assets/Frame 178.png" alt="" />
+    </footer>
+  );
+};
+
+export default Footer;

f7ae82eccd33a06bcbe7ed130180c2a0ffa2efd2
kareemHabeeb <omogbolahankareem16@gmail.com>
2026-05-16 23:42:38 +0100

    Folder Reorganizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import Pricing from "./Pages/About/Services/Pricing&Services";
+import Pricing from "./Pages/Services/Pricing&Services";
 
 const App = () => {
   return (
--- src/Pages/Services/Pricing&Services.jsx
+++ src/Pages/Services/Pricing&Services.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Pricing&Services.css";
-import Header from "../../../Components/Header/Header";
-import Button from "../../../Components/Props/Button";
-import Footer from "../../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
+import Button from "../../Components/Props/Button";
+import Footer from "../../Components/Footer/Footer";
 
 const Pricing = () => {
   return (
